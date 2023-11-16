7ec166504ae57cd82898c838fb42f71290560571
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-11-16 10:49:34 -0500

    created vite frontend, installed BrowserRouter & cleaned up files

---
+++ README.md
@@ -0,0 +1,1 @@
+# GeoCar Exchange Frontend
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Router></Router>
+    </div>
+  )
+}
+
+export default App
