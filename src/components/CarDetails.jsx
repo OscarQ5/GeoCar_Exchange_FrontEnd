0aa6260b8e512d3cc4437b755fb68c912aeb9d86
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-11-20 10:40:06 -0500

    using radar.io api to show distance between user & car

--- src/components/CarDetails.jsx
+++ src/components/CarDetails.jsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const API = import.meta.env.VITE_API_URL;
+const RADAR_API_KEY = import.meta.env.VITE_RADAR_API_KEY;
 
 export default function CarDetails() {
     const [car, setCar] = useState({});
     const [loading, setLoading] = useState(true);
+    const [userLocation, setUserLocation] = useState(null);
+    const [carLocation, setCarLocation] = useState(null);
+    const [distance, setDistance] = useState(null);
+
     const navigate = useNavigate();
     let { id } = useParams();
 
@@ -22,7 +27,96 @@ export default function CarDetails() {
         }
     };
 
-    useEffect(() => { fetchCar() }, [id]);
+    const getUserLocation = () => {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setUserLocation({ lat: latitude, lng: longitude });
+                },
+                (err) => { console.error(err); }
+            );
+        } else {
+            console.log('Geolocation is not supported in this browser.');
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetchCar();
+        };
+        fetchData();
+    }, [id]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            getUserLocation();
+            if (car.location) {
+                getCoordinatesFromAddress(car.location);
+            }
+        };
+
+        fetchData();
+    }, [car]);
+
+    const getCoordinatesFromAddress = (address) => {
+        const apiUrl = `https://api.radar.io/v1/geocode/forward?query=${encodeURIComponent(address)}`;
+
+        fetch(apiUrl, {
+            method: "GET",
+            headers: {
+                Authorization: `${RADAR_API_KEY}`,
+            },
+        })
+            .then(r => r.json())
+            .then((data) => {
+                if (data.addresses && data.addresses.length > 0) {
+                    const coordinates = data.addresses[0].geometry.coordinates
+                    setCarLocation(coordinates)
+                } else {
+                    console.error("No coordinates found for the address")
+                }
+            })
+            .catch(err => console.error(err));
+    }
+
+    useEffect(() => {
+        if (userLocation && carLocation) {
+            calculateDistance(userLocation, carLocation);
+        }
+    }, [userLocation, carLocation]);
+
+    const calculateDistance = (userLocation, carLocation) => {
+        const R = 6371;
+
+        const userLat = userLocation.lat;
+        const userLng = userLocation.lng;
+
+        let carLat, carLng;
+
+        if (Array.isArray(carLocation)) {
+            [carLng, carLat] = carLocation;
+        } else {
+            carLat = carLocation.lat;
+            carLng = carLocation.lng;
+        }
+
+        const dLat = deg2rad(carLat - userLat);
+        const dLon = deg2rad(carLng - userLng);
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(deg2rad(userLat)) * Math.cos(deg2rad(carLat)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = R * c;
+        setDistance(distance);
+    };
+
+    const deg2rad = (deg) => {
+        return deg * (Math.PI / 180);
+    }
+
+
 
     const handleDelete = () => {
         const isConfirmed = window.confirm("Are you sure you want to delete this car?");
@@ -50,7 +144,7 @@ export default function CarDetails() {
                 <p>Loading...</p>
             ) : (
                 <>
-                    <div style={{ display: "flex", alignItems: "center" }}>
+                    <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
                         <img
                             src={`https://geocar-exchange-backend.onrender.com${car.image_url}`}
                             alt={`Car: ${car.make} ${car.model}`}
@@ -78,6 +172,33 @@ export default function CarDetails() {
                             </Link>
                             <button className="button is-danger" onClick={handleDelete}>Delete</button>
                         </div>
+                        {distance !== null ? (
+                            <div style={{
+                                position: "absolute",
+                                top: "10px",
+                                left: "10px",
+                                background: "rgba(255, 255, 255, 0.8)",
+                                padding: "5px",
+                                borderRadius: "5px",
+                                fontSize: "1.2em",
+                                fontWeight: "bold",
+                            }}>
+                                Distance: {distance.toFixed(2)} miles
+                            </div>
+                        ) : (
+                            <div style={{
+                                position: "absolute",
+                                top: "10px", 
+                                left: "10px", 
+                                background: "rgba(255, 255, 255, 0.8)",
+                                padding: "5px",
+                                borderRadius: "5px",
+                                fontSize: "1.2em",
+                                fontWeight: "bold",
+                            }}>
+                                Calculating distance...
+                            </div>
+                        )}
                     </div>
                 </>
             )}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="container">
+        <div className="container" style={{marginTop: "470px"}}>
             <h2 className="title is-1">Welcome to the GeoCar Exchange 🏎️🏁</h2>
         </div>
     )
